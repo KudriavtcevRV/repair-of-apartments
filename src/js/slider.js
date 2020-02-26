315fbc48324bdade2d8c5be30bcb9e981e7ea9b0
KudriavtcevRV <ruslan_kudriavtcev@mail.ru>
2020-02-26 22:52:21 +0300

    Сверстал блок portfolio(слайдер) и сделал адаптив

---
+++ src/js/slider.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+	$('.slider').slick({
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	prevArrow: $('.arrows__left'),
+	nextArrow: $('.arrows__right'),
+	responsive: [{
+			breakpoint: 1200,
+			settings: {
+				arrows: false,
+				slidesToShow: 2,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+
+	]
+	})
+});
